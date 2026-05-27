8056e40bb4f5190e0f3e64c33ae398a1aef24eb8
digiyol.com <digiyol.com@DESKTOP-15RB4N3>
2026-05-27 10:09:07 -0700

    Add project files.

---
+++ bolboring finder SKF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace bolboring_finder_SKF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
